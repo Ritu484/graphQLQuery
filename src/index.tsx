4df472e92479a819a1b0070a038ac4ec82038fc2
Ritu484 <149618087+Ritu484@users.noreply.github.com>
2023-12-04 16:59:59 +0530

    Add files via upload

---
+++ src/index.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { FluentProvider, teamsLightTheme } from "@fluentui/react-components";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  gql,
+} from "@apollo/client";
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const client = new ApolloClient({
+  uri: "https://flyby-router-demo.herokuapp.com/",
+  cache: new InMemoryCache(),
+});
+// const client = ...
+
+client
+  .query({
+    query: gql`
+      query GetLocations {
+        locations {
+          id
+          name
+          description
+          photo
+        }
+      }
+    `,
+  })
+  .then((result) => console.log(result));
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <FluentProvider theme={teamsLightTheme}>
+      <App />
+    </FluentProvider>
+    ,
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
